3c4aa0b733a01ae455498a8ef03ca46513aaf4dc
Oleg Minenko <justprog2020@yandex.ru>
2023-02-20 22:48:35 +0300

    first commit

---
+++ animal-chipization/src/main/java/com/example/animalchipization/AnimalChipizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.animalchipization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalChipizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalChipizationApplication.class, args);
+    }
+
+}
---
+++ animal-chipization/src/test/java/com/example/animalchipization/AnimalChipizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalchipization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalChipizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
