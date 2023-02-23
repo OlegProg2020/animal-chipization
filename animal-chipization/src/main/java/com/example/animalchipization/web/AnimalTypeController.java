0421b0f45616e1bf2c59322034d85dd5902b335a
Oleg Minenko <justprog2020@yandex.ru>
2023-02-23 18:21:37 +0300

    added AnimalTypeRepository, AnimalTypeController

---
+++ animal-chipization/src/main/java/com/example/animalchipization/web/AnimalTypeController.java
@@ -0,0 +1,41 @@
+package com.example.animalchipization.web;
+
+import com.example.animalchipization.data.AnimalTypeRepository;
+import com.example.animalchipization.models.AnimalType;
+import com.example.animalchipization.models.LocationPoint;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/types", produces = "application/json")
+public class AnimalTypeController {
+
+    private final AnimalTypeRepository animalTypeRepository;
+
+    public AnimalTypeController(AnimalTypeRepository animalTypeRepository) {
+        this.animalTypeRepository = animalTypeRepository;
+    }
+
+    @GetMapping("/{typeId}")
+    public ResponseEntity<AnimalType> accountById(@PathVariable("typeId") Long typeId) {
+        ResponseEntity<AnimalType> response;
+        if (typeId <= 0) {
+            response = new ResponseEntity<>(null, HttpStatus.valueOf(400));
+        } else {
+            Optional<AnimalType> animalType = animalTypeRepository.findById(typeId);
+            if (animalType.isPresent()) {
+                response = new ResponseEntity<>(animalType.get(), HttpStatus.valueOf(200));
+            } else {
+                response = new ResponseEntity<>(null, HttpStatus.valueOf(404));
+            }
+        }
+        return response;
+    }
+
+}
